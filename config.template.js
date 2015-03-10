77863a78ee0eb0d7ed6f6a86bd5c618155a1df5d
erceth <erceth@gmail.com>
2015-03-09 20:08:54 -0600

    Update config.template.js

--- config.template.js
+++ config.template.js
@@ -1,7 +1,7 @@
 //rename this file to config.js, fill in your data and delete this line
 var config = {};
 
-config.twilioAccountDid = 'yourTwilioAccountSid';
+config.twilioAccountSid = 'yourTwilioAccountSid';
 config.twilioauthToken = 'yourTwilioAuthToken';
 config.wordOfDayPassword = 'yourWordOfDayPassword';
 config.twillioPhoneNumber = "yourTwillioPhoneNumber";
